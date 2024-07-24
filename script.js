baa828bc3ed220e670cc98de270bb887916a6d29
EversonNathanMendes <128055775+EversonNathanMendes@users.noreply.github.com>
2024-07-24 10:05:52 -0400

    assa

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// Mensagem de boas-vindas ao clicar no botão document.addEventListener('DOMContentLoaded', () => { const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    alert('Estamos animados para começar o ano com vocês!'); });
+// Mensagem de boas-vindas ao clicar no botão 
+document.addEventListener('DOMContentLoaded', () => { 
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+     alert('Estamos animados para começar o ano com vocês!'););
+    }):
+
+
     // Contador de dias para o início das aulas
-    const startDate = new Date('2024-08-01'); // Defina a data de início das aulas
+const startDate = new Date('2024-08-01'); // Defina a data de início das aulas
 const today = new Date();
 const timeDiff = startDate - today;
 const daysLeft = Math.ceil(timeDiff/(1000*60*60*24));
 
 
 const countdown Element = document.createElement('p');
-countdownElement.textContent = 'Faltam $(daysLeft) dias para o início das aulas!";
+countdownElement.textContent = 'Faltam $(daysLeft) dias para o início das aulas!`;
 document.querySelector('.card').appendChild(countdownElement);
 
 
@@ -17,6 +22,7 @@ document.querySelector('.card').appendChild(countdownElement);
 const image = document.querySelector('img');
 image.addEventListener('click', () => {
  image.src = voltaaulas.jpg;
+
 });
 
 
@@ -28,5 +34,5 @@ card.style.transition = 'opacity 2s';
 
 setTimeout(() => {
     card.style.opacity
- }, 100);
+  }, 100);
 });
